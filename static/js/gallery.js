e633751075f6b29eb3601561a00d194b84fe967d
Benjamin Friedman <friedman.benjamin@gmail.com>
2021-12-06 22:03:16 +0100

    sending everything, testing continued sends

---
+++ static/js/gallery.js
@@ -0,0 +1,43 @@
+(function() {
+  if(window.openFullImg) {
+    throw new "openFullImg already defined in this context!";
+  }
+
+  window.openFullImg = function(id) {
+
+    let overlay = document.getElementById("overlay");
+    let overlayImg = document.getElementById("overlay-img");
+
+    if(!overlay || !overlayImg) {
+      // create new overlay first
+      let overlay = "<div id='overlay' class='' onclick='hideOverlay();'>";
+      overlay += "<img id='overlay-img' src='" + id + "'>";
+      overlay += "<a href='#' onclick='hideOverlay();'>Close</a>";
+      overlay += "</div>";
+      let body = document.getElementsByTagName("body")[0];
+      body.insertAdjacentHTML('afterbegin', overlay);
+
+      overlay = document.getElementById("overlay");
+      overlayImg = document.getElementById("overlay-img");
+
+      // sketchy js
+      overlayImg.src = id;
+      overlay.className = ""; // no-hide
+
+    } else {
+      // sketchy js
+      overlayImg.src = id;
+      overlay.className = ""; // no-hide
+
+    }
+  }
+
+  window.hideOverlay = function() {
+    let overlay = document.getElementById("overlay");
+    let overlayImg = document.getElementById("overlay-img");
+
+    overlay.className = "hidden";
+    overlayImg.src = "";
+  }
+
+})();
